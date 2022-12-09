ac57c6fcbd19be66be958e9cbc97fd1267301634
gloriane98 <glorianek66@gmail.com>
2022-12-09 12:40:43 +0100

    first commit

---
+++ components/Main.jsx
@@ -0,0 +1,60 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+import { FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
+const Main = () => {
+  return (
+    <div id='home' className='w-full h-screen text-center'>
+      <div className='max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center'>
+        <div>
+          <p className='uppercase text-sm tracking-widest text-[#1d1d1d]'>
+            LET&#39;S BUILD SOMETHING TOGETHER
+          </p>
+          <h1 className='py-4 text-[#1d1d1d]'>
+            Hi, I&#39;m <span className='text-[#6D493B]'> Gloriane Kingolo</span>
+          </h1>
+          <h1 className='py-2 text-[#1d1d1d]'>Full Stack Developer</h1>
+          <p className='py-4 text-[#1d1d1d] sm:max-w-[70%] m-auto'>
+          Digital solutions developer,UX and
+          UI designer & graphic designer.
+          Technology, software development and design are my passions.
+          </p>
+          <div className='flex items-center justify-between max-w-[330px] m-auto py-4'>
+            <a
+              href='https://www.linkedin.com/in/gloriane-kingolo/'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <FaLinkedinIn />
+              </div>
+            </a>
+            <a
+              href='https://github.com/gloriane98'
+              target='_blank'
+              rel='noreferrer'
+            >
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <FaGithub />
+              </div>
+            </a>
+            <Link href='/#contact'>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <AiOutlineMail />
+              </div>
+            </Link>
+            <Link href='/resume'>
+              <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                <BsFillPersonLinesFill />
+              </div>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Main;
